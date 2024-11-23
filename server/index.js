435e908ca31e72d58c9ed39cb3d4c4fd52da5637
Paras Tuteja <162349754+parastuteja@users.noreply.github.com>
2024-11-23 21:21:36 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express= require ('express');
+const cors =require('cors')
+const mongoose = require ("mongoose")
+
+
+const app=express();
+require ('dotenv').config()
+app.use(cors())
+app.use(express.json())
+mongoose.connect(process.env.MONGO_URL,{ 
+}).then(()=>{
+    console.log("db is connected")
+}).catch((err)=>{
+    console.log(err.message)
+})
+const server =app.listen(process.env.port,()=>{
+    console.log(`server is connected on port${process.env.port}`)
+})
