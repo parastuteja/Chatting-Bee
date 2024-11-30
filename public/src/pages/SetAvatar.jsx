16b1ab024a4f4cf935bb7837e9bc7ca54768a462
Paras Tuteja <162349754+parastuteja@users.noreply.github.com>
2024-11-30 16:02:40 +0530

    update

--- public/src/pages/SetAvatar.jsx
+++ public/src/pages/SetAvatar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
-function SetAvatar() {
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Loader from '../Assets/Loader.gif'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import{setAvatarRoute}from'../utils/APIroutes'
+import { useNavigate } from "react-router-dom";
+const api='https://api.multiavatar.com/456789'
+const navigate= useNavigate()
+export default function SetAvatar() {
   return (
-    <div>SetAvatar</div>
+    <aContainer>
+      SetAvatar
+    </aContainer>
+  
   )
 }
 
-export default SetAvatar
