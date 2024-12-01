0a1da35f02763ead12cf0dcf46d46d6ac8f9ddf1
Paras Tuteja <162349754+parastuteja@users.noreply.github.com>
2024-12-01 20:27:46 +0530

    Update SetAvatar.jsx

--- public/src/pages/SetAvatar.jsx
+++ public/src/pages/SetAvatar.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SetAvatarRoute } from '../utils/APIroutes'
-
+import { useNavigate } from 'react-router-dom'
+import Loader from'../Assets/Loader.gif'
+import { ToastContainer } from 'react-toastify'
 export default function SetAvatar(){
-  return(
-    <div className='Avatar'>
-    
-    
+  const api='https://api.multiavatar.com/456789'
+  const navigate=useNavigate()
+const [avatars,setAvatars]=useState([])
+const[setloading,setisLoading]=useState(true)
+const[selectedAvatar,setSelectedAvatar]=useState(undefined)
+const toastOptions = {
+  position: "bottom-right",
+  autoClose: 8000,
+  pauseOnHover: true,
+  draggable: true,
+  theme: "dark",
+};
+const setProfilePicture=async()=>{
+  
+}
+  return( <>    
+  <aComponent>
+    <div className="title-container">
+      <h1>
+        pick an avatar as ur profile picture
+      </h1>
+      <div className="avatars">
+        {
+
+        }
+      </div>
     </div>
+    </aComponent>
+    <ToastContainer/>
+    </>
+
   )
 }
