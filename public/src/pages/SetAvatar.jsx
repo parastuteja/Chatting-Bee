cdb818b0520f406ffde013dfaf34f0288c9a4afc
Paras Tuteja <162349754+parastuteja@users.noreply.github.com>
2024-12-05 12:28:29 +0530

    Update SetAvatar.jsx

--- public/src/pages/SetAvatar.jsx
+++ public/src/pages/SetAvatar.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import loader from'../Assets/Loader.gif'
 import { setAvatarRoute } from '../utils/APIroutes'
+import { useNavigate } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import axios from 'axios'
 export default function SetAvatar() {
+  const api='https://multiavatar.com/45678945'
+  const navigate=useNavigate();
+  const [avatars, setAvatars] = useState([])
+  const [selectedAvatar, setSelectedAvatar] = useState(undefined)
+  const [isLoading, setIsLoading] = useState(true)
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+  const setProfilePicture=async()=>{
+
+  }
+  useEffect( async()=>{
+    const data=[]
+    for(let i=0;i<4;i++){
+      const image=await axios.get(`${api}/${Math.round(Math.random()*1000)}`)
+    const buffer=new Buffer(image.data)
+    data.push(buffer)
+  }},[])
   return (
-    <div>SetAvatar</div>
+    <>
+    <aContainer>
+      <div className="title-container">
+        <h1>Pick An Avatar For Your Profile Picture</h1>
+      </div>
+      <div className="avatars">
+
+      </div>
+       Set Your Avatar
+    </aContainer>
+    <ToastContainer/>
+    </>
   )
 }
 
