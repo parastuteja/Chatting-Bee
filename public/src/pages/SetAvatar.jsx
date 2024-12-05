f3dcd317b45e1c42cbbc8c8f5d0dd636465a77f9
Paras Tuteja <162349754+parastuteja@users.noreply.github.com>
2024-12-05 12:14:53 +0530

    update

--- public/src/pages/SetAvatar.jsx
+++ public/src/pages/SetAvatar.jsx
@@ -1,54 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import { SetAvatarRoute } from '../utils/APIroutes'
-import { useNavigate } from 'react-router-dom'
-import Loader from'../Assets/Loader.gif'
-import { ToastContainer } from 'react-toastify'
-import axios from 'axios'
-import { base } from '../../../server/model/userModel'
-export default function SetAvatar(){
-  const api='https://api.multiavatar.com/456789'
-  const navigate=useNavigate()
-const [avatars,setAvatars]=useState([])
-const[setloading,setisLoading]=useState(true)
-const[selectedAvatar,setSelectedAvatar]=useState(undefined)
-const toastOptions = {
-  position: "bottom-right",
-  autoClose: 8000,
-  pauseOnHover: true,
-  draggable: true,
-  theme: "dark",
-};
-const setProfilePicture=async()=>{}
-useEffect(()=>{
-const data=[]
-for(let i=0;i<4;i++){
-const image=  await axios.get (`${api}/${Math.round(Math.random() * 1000)}`);
-const buffer=new Buffer(image.data)
-data.push(buffer.toString('base64'))
-}
-setAvatars(data)
-setisLoading(false)
-},[])
-  return( <>    
-  <aComponent>
-    <div className="title-container">
-      <h1>
-        pick an avatar as ur profile picture
-      </h1>
-      <div className="avatars">
-        {
-avatars.map((avatars,index)=>{
-  return(`avatar ${selectedAvatar ===index?"selected":''}`)
-}) }
-      </div>
-      <img  src={`data:image/svg+xml;base64,${avatar}`}
-      alt='avatar'
-      key={avatar}
-      onClick={()=>setSelectedAvatar(index)}/>
-    </div>
-    </aComponent>
-    <ToastContainer/>
-    </>
-
+import React from 'react'
+import loader from'../Assets/Loader.gif'
+import { setAvatarRoute } from '../utils/APIroutes'
+export default function SetAvatar() {
+  return (
+    <div>SetAvatar</div>
   )
 }
+
