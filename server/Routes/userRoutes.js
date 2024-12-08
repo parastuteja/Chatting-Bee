1a1f4c337a624f6a48c14a72be4651afa571c8f1
Paras Tuteja <162349754+parastuteja@users.noreply.github.com>
2024-12-08 13:13:33 +0530

    update

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -1,8 +1,8 @@
 const SetAvatar = require('../../public/src/pages/SetAvatar.jsx');
 const { register } = require("../Controller/userController")
 const { login } = require("../Controller/userController")
-const express=require("express")
-const router=require('express').Router()
+const express =require('express')
+const router=express.Router()
 router.post("/Register",register)
 router.post("/Login",login)
 router.post('/SetAvatar',SetAvatar)
