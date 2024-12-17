492471655d81c7e680b0c9326ac23fd44375b970
Paras Tuteja <162349754+parastuteja@users.noreply.github.com>
2024-12-17 21:26:44 +0530

    update

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useRef, useState } from 'react';
-import axios from 'axios'
-import navigate from 'navigate';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-
-import { alluserRoutes, host } from '../utils/APIroutes';
 import Contacts from '../Components/Contacts';
+import styled from "styled-components";
+import { alluserRoutes } from '../utils/APIroutes';
+
 export default function Chat() {
-  const socket=useRef()
-  const navigate=useNavigate()
-  const [contacts,setContacts]=useState([])
-  const[currentUser,setCurrentUser]=useState([])
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([]);
+  const [currentUser , setCurrentUser ] = useState([]);
+  const [currentChat, setCurrentChat] = useState('');
+
   useEffect(() => {
     const checkUser  = async () => {
       const userData = localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY);
@@ -20,68 +20,59 @@ export default function Chat() {
         setCurrentUser (JSON.parse(userData));
       }
     };
-  
+
     checkUser ();
   }, [navigate]);
-  useEffect(()=>{
- const checkAvatar= async()=>{
-  if(currentUser.isAvatarImageSet){
-    const data=await axios.get(`${alluserRoutes}/${currentUser._id}`)
-    setContacts(data.data)
 
-  }
-  else{
-    navigate('/setAvatar')
-  }
- }    
-  })
- 
+  useEffect(() => {
+    const checkAvatar = async () => {
+      if (currentUser  && currentUser._id) {
+        if (currentUser.isAvatarImageSet) {
+          try {
+            const { data } = await axios.get(`${alluserRoutes}/${currentUser._id}`);
+            setContacts(data);
+          } catch (error) {
+            console.error("Error fetching contacts:", error);
+          }
+        } else {
+          navigate('/setAvatar');
+        }
+      }
+    };
+
+    checkAvatar();
+  }, [currentUser , navigate]);
+
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  };
+
   return (
-    <div className="h-screen flex bg-gradient-to-r from-blue-50 to-indigo-100">
-      {/* Contacts List */}
-      <div className="w-1/4 bg-purple-50 shadow-md border-r border-gray-300 overflow-y-auto">
-        <div className="p-4 border-b border-gray-300 bg-purple-600 text-white">
-          <h2 className="text-lg font-bold">Contacts</h2>
-        </div>
-       <Contacts contacts={contacts} currentUser={contacts}/>
+    <Container>
+      <div className="container">
+        <Contacts contacts={contacts} changeChat={handleChatChange} />
       </div>
-
-      {/* Chat Window */}
-      <div className="w-3/4 flex flex-col bg-indigo-50">
-        <div className="p-4 border-b border-gray-300 bg-indigo-700 text-white">
-          <h2 className="text-lg font-bold">Chat with Alice</h2>
-        </div>
-
-        <div className="flex-1 p-4 overflow-y-auto bg-white">
-          {/* Messages */}
-          <div className="space-y-4">
-            <div className="flex">
-              <div className="bg-indigo-500 text-white p-3 rounded-lg max-w-xs shadow-md">
-                Hi there!
-              </div>
-            </div>
-            <div className="flex justify-end">
-              <div className="bg-purple-200 text-purple-700 p-3 rounded-lg max-w-xs shadow-md">
-                Hello! How are you?
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* Input Box */}
-        <div className="p-4 border-t border-gray-300 bg-indigo-200 flex items-center">
-          <input
-            type="text"
-            placeholder="Type a message..."
-            className="flex-1 border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-indigo-500 shadow-sm"
-          />
-          <button className="ml-4 px-4 py-2 bg-indigo-500 text-white rounded-lg hover:bg-indigo-600 shadow-md">
-            Send
-          </button>
-        </div>
-      </div>
-    </div>
+    </Container>
   );
 }
 
-
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
--- server/Controller/userController.js
+++ server/Controller/userController.js
@@ -1,75 +1,79 @@
-const User=require('../model/userModel')
+const User = require('../model/userModel');
 const bcrypt = require("bcrypt");
-
 
 module.exports.register = async (req, res, next) => {
     try {
-      const { username, email, password } = req.body;
-      const usernameCheck = await User.findOne({ username });
-      if (usernameCheck)
-        return res.json({ msg: "Username already used", status: false });
-      const emailCheck = await User.findOne({ email });
-      if (emailCheck)
-        return res.json({ msg: "Email already used", status: false });
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const user = await User.create({
-        email,
-        username,
-        password: hashedPassword,
-      });
-      delete user.password;
-      return res.json({ status: true, user });
+        const { username, email, password } = req.body;
+        const usernameCheck = await User.findOne({ username });
+        if (usernameCheck)
+            return res.json({ msg: "Username already used", status: false });
+        
+        const emailCheck = await User.findOne({ email });
+        if (emailCheck)
+            return res.json({ msg: "Email already used", status: false });
+        
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            email,
+            username,
+            password: hashedPassword,
+        });
+        
+        delete user.password;
+        return res.json({ status: true, user });
     } catch (err) {
-      next(err);
+        next(err);
     }
-  };
+};
 
 module.exports.login = async (req, res, next) => {
     try {
-      const { username,  password } = req.body;
-      const users = await User.findOne({ username });
-      if (!users)
-        return res.json({ msg: "User Not Found Please Register Yourself", status: false });
-      const isValidPassword = await bcrypt.compare(password, users.password);
-      if(!isValidPassword){return res.json(
-        {msg:'bad password please try again' , status:false})
-      }
-      const hashedPassword = await bcrypt.hash(password, 10);
-     
-      delete users.password;
-      return res.json({ status: true, users });
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if (!user)
+            return res.json({ msg: "User  Not Found. Please Register Yourself", status: false });
+        
+        const isValidPassword = await bcrypt.compare(password, user.password);
+        if (!isValidPassword) {
+            return res.json({ msg: 'Bad password, please try again', status: false });
+        }
+        
+        delete user.password;
+        return res.json({ status: true, user });
     } catch (err) {
-      next(err);
+        next(err);
     }
-  };
-  module.exports.setAvatar =async (req,res) => {
-    try{
-const userid=req.params.id;
-const avatar=req.body.image;
-const avatarImage=req.body.image
-const userData=await User.findByIdAndUpdate(userid,{
-  isAvatarImageSet:true,
-  avatarImage,
-})
-return req.json({
-  isSet: userData.isAvatarImageSet,
-  image:userData.avatarImage,
-})
-    }catch(ex){
-      next(ex)
-    }
-  }
-  module.exports.getAllUsers = async (req, res, next) => {
+};
+
+module.exports.setAvatar = async (req, res, next) => {
     try {
-      const users = await User.find({ _id: { $ne: req.params.id } }).select([
-        "email",
-        "username",
-        "avatarImage",
-        "_id",
-      ]);
-      return res.json(users);
-    } catch (ex) {
-      next(ex);
+        const userId = req.params.id;
+        const avatarImage = req.body.image;
+        
+        const userData = await User.findByIdAndUpdate(userId, {
+            isAvatarImageSet: true,
+            avatarImage,
+        }, { new: true }); // Return the updated document
+        
+        return res.json({
+            isSet: userData.isAvatarImageSet,
+            image: userData.avatarImage,
+        });
+    } catch (err) {
+        next(err);
     }
-  };
-  
+};
+
+module.exports.getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find({ _id: { $ne: req.params.id } }).select([
+            "email",
+            "username",
+            "avatarImage",
+            "_id",
+        ]);
+        return res.json(users);
+    } catch (err) {
+        next(err);
+    }
+};
